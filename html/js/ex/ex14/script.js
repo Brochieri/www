c42e65a54ec3b21b4435b4fbe7c73920e525b0d8
André Luís Silva Brochieri <alsbrochieri@gmail.com>
2025-01-20 15:30:29 -0300

    alteração do dia 20/01

---
+++ html/js/ex/ex14/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    msg.innerHTML = `Agora são ${hora} horas`;
+    if (hora >= 0 && hora < 12) {
+        //bom dia 
+        img.src = "./img/fotobomdia.png";
+        document.body.style.background = "#b1bcb2";
+    } else if (hora >= 12 && hora <18) {
+        //boa tarde
+        img.src = "./img/fotoboatarde.png";
+        document.body.style.background = "#b0e2bf";
+    } else {
+        //boa noite
+        img.src = "./img/fotoboanoite.png";
+        document.body.style.background = "#1d2637";
+    }
+}
